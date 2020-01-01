023f825b4067c7fadbbf478fcc935ac6383af441
xiaoyueyue165 <1656800216@qq.com>
2020-01-02 01:26:53 +0800

    upd: issues.json

--- helpers/issues.ts
+++ helpers/issues.ts
@@ -1,34 +1,68 @@
-import { formatMarkdown, check_npm_argv } from '../src/utils';
-import { axios, cheerio, turndownService } from './index';
+import { $axios } from './index';
+import { createFile, saveData_dev } from '../src/utils';
+const table = require('markdown-table');
 
-// 单个博客地址
-let issues_url = 'https://github.com/yanyue404/blog/issues/110';
-
-// 命令行传参校验
-check_npm_argv(
-  'log:issues',
-  issues_url,
-  'npm argv Error,请输入一个合理的 GitHub issues 地址，比如: https://github.com/yanyue404/blog/issues/111',
-);
-
-function getIssues(fetchUrl: string) {
-  return axios
-    .get(fetchUrl)
-    .then(function(response: any) {
-      let html_string = response.data.toString(); // 获取网页内容
-      const $ = cheerio.load(html_string); // 传入页面内容
-      const content = turndownService.turndown($('table').html());
-      return content;
-    })
-    .catch((err: any) => {
-      console.log(err);
-    });
+function getAPI(url: string) {
+  $axios.get(url).then((result: any) => {
+    console.log(result.data.length);
+    console.log(result.headers.link);
+    getResult(getPageRequestList(result.headers.link));
+  });
 }
 
-getIssues(issues_url)
-  .then((markdown: string) => {
-    console.log(formatMarkdown(markdown));
-  })
-  .catch((err: any) => {
-    console.log(err);
-  });
+function getPageRequestList(str: string) {
+  let lastPage = str
+    .match(/<(.+?)>/g)[1]
+    .slice(1, -1)
+    .split('=');
+  let result = [];
+  let href = lastPage[0];
+  for (let i = 1; i <= Number(lastPage[1]); i++) {
+    result.push(href + '=' + i);
+  }
+  return result;
+}
+
+function getResult(fetchList: string[]) {
+  let obj: any = [];
+  let issues_obj: any = [];
+  let tableArr: any = [['Avatar', 'UserName', 'Repos_url']];
+  let getAvatorImg = (src: string, name: string) => {
+    return `<img class="avatar ghh-user-x tooltipstered" height="50" width="50" alt="${name}" src="${src}" style="box-shadow: transparent 0px 0px;">`;
+  };
+  return Promise.all(fetchList.map(url => $axios.get(url))).then(
+    (...res: any) => {
+      let issues = res[0];
+      for (let m = 0; m < issues.length; m++) {
+        obj = obj.concat(issues[m].data);
+      }
+      for (let n = 0; n < obj.length; n++) {
+        let issue_simgle = obj[n];
+        console.log(issue_simgle);
+
+        // tableArr.push([
+        //   getAvatorImg(issue_simgle.avatar_url, issue_simgle.login),
+        //   `[${issue_simgle.login}](${issue_simgle.html_url})`,
+        //   issue_simgle.repos_url,
+        // ]);
+        issues_obj.push({
+          id: issue_simgle.number,
+          title: issue_simgle.title,
+          labels: issue_simgle.labels[0].name,
+          created_at: issue_simgle.created_at,
+          updated_at: issue_simgle.updated_at,
+          body_html: issue_simgle.body_html,
+        });
+      }
+      // console.log(issues_obj);
+      // const content = table(tableArr, {
+      //   align: ['c', 'c', 'l'],
+      // });
+      // createFile('docs/', 'following.md', content);
+
+      saveData_dev(issues_obj, 'issues.json');
+    },
+  );
+}
+
+getAPI('https://api.github.com/repos/yanyue404/blog/issues');
